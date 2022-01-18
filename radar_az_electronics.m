1ec5b5b73942da38feedd4628a95f5cb06649407
Burak Alanyalıoğlu <89913513+felsefesinde@users.noreply.github.com>
2022-01-18 16:07:27 +0300

    Update radar_az_electronics.m

--- radar_az_electronics.m
+++ radar_az_electronics.m
@@ -4,9 +4,9 @@
 % Copyright 2016 The MathWorks, Inc
 clear all;
 %% Create an Arduino object with the necessary add-ons.
-a = arduino('COM3', 'Uno', 'Libraries', {'Ultrasonic', 'Servo'});
+a = arduino('COM6', 'Uno', 'Libraries', {'Ultrasonic', 'Servo'});
 
-%% Create an ultrasonic sensor object with trigger pin D12 and echo pin D13.
+%% Create an ultrasonic sensor object with trigger pin D12 and echo pin D13 (By checking the "L" LED on your Arduino Uno which is connected to D13 of your board, you can observe if the gating of your ultrasonic sensor is successful or not.).
 %  Create a servo object for the servo connected to pin D3. 
 sensor = ultrasonic(a, 'D12', 'D13');
 servo_motor = servo(a, 'D3');
@@ -16,7 +16,7 @@ servo_motor = servo(a, 'D3');
 %  via the bounceback time of the ultrasonic ping. Take two measurements
 %  and average them for accuracy. Record the angle and the distance (in cm)
 i = 1;
-table = zeros(180,2);
+table_pos = zeros(180,2);
 for theta = 0 : 1/180 : 1
     writePosition(servo_motor, theta);
     dist1 = readDistance(sensor);
@@ -26,7 +26,12 @@ for theta = 0 : 1/180 : 1
     table(i,1) = (i-1);
     table(i,2) = round(dist * 100,2);
     i = i + 1;
+    polarplot (table(:,1)*pi/180, table (:,2), "r");
+    title('Ploarplot Map of the Environment');
+    thetalim([0 180]);
+    grid on;
 end
+clf()
 %  Rotate the servo motor from 180 to 0 degrees. Replace the values in the
 %  table with the average of the clockwise and counterclockwise scans to
 %  improve the accuracy of the map.
@@ -39,12 +44,17 @@ for theta = 1 : -1/180 : 0
     dist = (dist1+dist2)/2;
     table(i-j,2) = (table(i-j,2) + round(dist * 100,2))/2;
     j = j + 1;
+    polarplot (table(:,1)*pi/180, table (:,2));
+    title('Polarplot Map of the Environment');
+    thetalim([0 180]);
+    grid on;
 end
 
 %% Make a polar plot of the distance data to display the map. 
 %  Limit the theta values to be between 0 and 180 because the map is only
 %  for obstacles in front of the servo. 
-polarplot (table(:,1)*pi/180, table (:,2));
-title('Map of the Environment');
+figure(2)
+plot (table(:,1)*pi/180, table (:,2),"g");
+title('Average Plot of the Environment');
 thetalim([0 180]);
 grid on;
